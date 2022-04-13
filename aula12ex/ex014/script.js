a76e241f912d0a3a88e235aa668e31a3c73bc9f9
EdnaldoTeofilo <ednaldofilho14@gmail.com>
2022-04-13 10:39:07 -0300

    exercícios com condições

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#fafdfd'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#ebba71'
+    } else {
+        //BOA NOITE!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#08284c'
+    }
+}
